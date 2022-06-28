8be22cd3c43f818931f580249aa56c19f89b06ce
Dmitry <gonokhov.dm@gmail.com>
2022-06-28 16:43:54 +0700

    fill month input is done

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const App = {
 		return {
 			title: 'Test',
 			cardNumber: ['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'],
-			holderName: 'FULL NAME'
+			holderName: 'FULL NAME',
+			monthDate: 'MM',
+			yearDate: 'YY'
 		}
 	},
 	methods: {
@@ -16,7 +18,7 @@ const App = {
 			for (let i = 0; cardNumber.length <= 16; i++) {		
 				changeCardNumber = cardNumber.splice([i], 1, cardNumber[i]);
 				if (cardNumber[i] == undefined) {
-					cardNumber[i] = '#';
+					cardNumber[i] = '#'
 				}
 				this.cardNumber = cardNumber;
 			}	
@@ -27,12 +29,45 @@ const App = {
 		inputChangeName(event) {
 			event.target.value = event.target.value.toUpperCase();
 			if (event.target.value == '') {
-				this.holderName = 'FULL NAME';
-			} else {this.holderName = event.target.value}			
+				this.holderName = 'FULL NAME'
+			} else { this.holderName = event.target.value }			
+		},
+
+		// Change display MONTH
+		// Only numbers from 01 to 12
+		inputChangeMonth(event) {
+			// 1 <= input <= 12
+			if ((event.target.value < 1) && (event.target.value != '')) {
+				event.target.value = 1;
+			} else {
+				if (event.target.value > 12) {
+					event.target.value = 12;
+				}
+			}
+
+			// add '0' after the once number
+			// fill card place 'MM' if input is clear
+			if (event.target.value < 10) {
+				this.monthDate = '0' + event.target.value
+			} else {
+				if (event.target.value == '') {
+					this.monthDate = 'MM'
+				} else {
+					this.monthDate = event.target.value		
+				}
+			}			
+		},
+
+		// Change display YEAR
+		// Only numbers more then 22
+		inputChangeYear(event) {
+			let yearNow = new Date();			
+			let shortYearNow = yearNow.getFullYear().toString();
+			// get 2 last numbers of year
+			shortYearNow = shortYearNow[2] + shortYearNow[3];
 		}
 	}
 }
-
 
 Vue.createApp(App).mount('#appPayment')
 
