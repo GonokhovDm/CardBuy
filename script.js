d9b6dc376daa9c2b0b7d0f7e567d7c9dc1ac55bd
Dmitry <gonokhov.dm@gmail.com>
2022-06-28 14:12:03 +0700

    number & name input is done

--- script.js
+++ script.js
@@ -2,16 +2,63 @@ const App = {
 	data() {
 		return {
 			title: 'Test',
-			cardNumber: '################'
+			cardNumber: ['#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'],
+			holderName: 'FULL NAME'
 		}
 	},
 	methods: {
-		inputChangeHandler(event) {
-			let input = [];
-			input = event.target.value;
-			this.cardNumber = input.replace(/(\d{1,4}(?=(?:\d\d\d\d)+(?!\d)))/g, "$1" + ' ');
+		// Change display CART NUMBER
+		// Not more then 16 chars
+		// Only numbers - function validateNumber(evt) {}
+		inputChangeNumber(event) {
+			cardNumber = event.target.value.split('');	
+			let changeCardNumber = [];			
+			for (let i = 0; cardNumber.length <= 16; i++) {		
+				changeCardNumber = cardNumber.splice([i], 1, cardNumber[i]);
+				if (cardNumber[i] == undefined) {
+					cardNumber[i] = '#';
+				}
+				this.cardNumber = cardNumber;
+			}	
+		},
+
+		// Change display HOLDER NAME
+		// Only latin letters - function validateName(evt) {}
+		inputChangeName(event) {
+			event.target.value = event.target.value.toUpperCase();
+			if (event.target.value == '') {
+				this.holderName = 'FULL NAME';
+			} else {this.holderName = event.target.value}			
 		}
 	}
 }
 
+
 Vue.createApp(App).mount('#appPayment')
+
+// Check! 
+// Only numbers on input Number
+function validateNumber(evt) {
+	var theEvent = evt || window.event;
+	var key = theEvent.keyCode || theEvent.which;
+	key = String.fromCharCode( key );
+	var regex = /[0-9]/;
+	if( !regex.test(key) ) {
+	  theEvent.returnValue = false;
+	  if(theEvent.preventDefault) theEvent.preventDefault();
+	}
+ }
+
+
+// Check!
+// Only letters on input Name
+function validateName(evt) {
+	var theEvent = evt || window.event;
+	var key = theEvent.keyCode || theEvent.which;
+	key = String.fromCharCode( key );
+	var regex = /[A-Za-z\s]/;
+	if( !regex.test(key) ) {
+	  theEvent.returnValue = false;
+	  if(theEvent.preventDefault) theEvent.preventDefault();
+	}
+ }
